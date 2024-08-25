24020ae6a4728058ae98236e7047d39ca3eba82e
Courtney LaCroix <courtney.lacroix@outlook.com>
2024-08-25 14:04:55 -0600

    commit from Friday's class

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,9 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		// commmmments
+		System.out.println("\"Hello World!\"");
 		System.out.println("My name is Courtney~");
-
+		
+			
 	}
 
 }
---
+++ Math/src/Variables.java
@@ -0,0 +1,19 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		int num2;
+		
+		
+		num2 = 4;
+		
+		int num3 = num1 + num2;
+		
+		
+		System.out.println("The number is:\t" + "\"" + num3 + "\"");
+
+	}
+
+}
