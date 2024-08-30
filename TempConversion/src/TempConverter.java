e20c2bb757828b94ac2455c17ff73d87d94fc181
Courtney LaCroix <courtney.lacroix@outlook.com>
2024-08-30 15:28:12 -0600

    Updated Tempurature converter for class assignment

--- TempConversion/src/TempConverter.java
+++ TempConversion/src/TempConverter.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
 
 public class TempConverter {
 
 	public static void main(String[] args) {
+	
+	Scanner scn;
+	scn = new Scanner(System.in);
 		
-	double fah = 75;
+	System.out.println("Lets convert the tempurature!");
+	System.out.println("What is the tempurature in Fahrenheit?");
+	double fah = scn.nextDouble();
 	double cel = (fah - 32) * (5.0/9);
 	
-	double cels = 25;
+
+	System.out.printf("It is %.2f degree's in Celsius\n",cel);
+	
+	
+	System.out.println("Lets convert the tempurature again!");
+	System.out.println("What is a NEW tempurature in Celsius?");
+	double cels = scn.nextDouble();
 	double fahren = cels * (9.0/5) + 32;
 	
 	
-	System.out.println(cel);
-	System.out.println(fahren);
+	System.out.printf("It is %.2f degree's in Fahrenhiet\n",fahren);
 	
 	
 
