5db54d9051f11f49109cf8dcec0e4d126bcb1e11
Courtney LaCroix <courtney.lacroix@outlook.com>
2024-09-06 11:22:39 -0600

    Worked on interative greeter assignment

---
+++ InteractiveGreeter/src/GreeterAndQuestion.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class GreeterAndQuestion {
+
+	public static void main(String[] args) {
+		Scanner scn;
+		scn = new Scanner(System.in);
+		
+		String name;
+		String originalLocation;
+		int age;
+		String hobby;
+		double donationAmount;
+
+		
+		System.out.println("What is your name?\n");
+		name = scn.nextLine();
+		
+		System.out.println("Where are you from?\n");
+		originalLocation = scn.nextLine();
+		
+		System.out.println("How old are you?\n");
+		age = scn.nextInt();
+		
+		System.out.printf("Hello %s, you are from %s and are %d years old!\n", name, originalLocation, age);
+		
+		System.out.println("What is one of your favorite hobbies?\n");
+		hobby = scn.next();
+		
+		
+		System.out.printf("That's really cool that you like %s!\n", hobby);
+		
+		System.out.println("How much would you like to donate today?\n");
+		donationAmount = scn.nextDouble();
+		
+		System.out.printf("Thank you for donating $%.2f! Have a great day!", donationAmount);
+	
+	}
+
+}
