34a1d5b7f7cef5f7921969f9d5d18f0448e3e583
Courtney LaCroix <courtney.lacroix@outlook.com>
2024-09-13 09:21:45 -0600

    Circle Calc project

---
+++ CircleCalculation/src/CalculateCircle.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CalculateCircle {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		double userInput;
+		
+		System.out.println("Please enter the radius of your circle:");
+		
+		userInput = scn.nextDouble();
+		
+		System.out.printf("You have entered the radius: %.2f \n", userInput);
+
+		System.out.printf("Your circles diameter is %.2f, area is %.2f and circumfrence is %.2f",
+				getDiameter(userInput),getArea(userInput), getcircumference(userInput));
+	}
+	
+public static double getDiameter(double userInput) {
+	 double diaResult = userInput * 2;
+	 return diaResult;
+}
+
+public static double getArea(double userInput) {
+	double areaResult = Math.PI * Math.pow(userInput, 2);
+	return areaResult;
+}
+
+public static double getcircumference(double userInput) {
+	double circumfResult = 2 * Math.PI * userInput;
+	return circumfResult;
+}
+	
+}
--- CodingBat/src/Logic.java
+++ CodingBat/src/Logic.java
@@ -2,8 +2,33 @@
 public class Logic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
+	public int caughtSpeeding(int speed, boolean isBirthday) {
+	  if (speed <= 60 || isBirthday && (speed <= 65)){
+	    return 0;
+	  }
+	  if (speed <= 61 && >= 80 || isBirthday && (speed >= 85)){
+	    return 1;
+	  }
+	  if (speed >= 81 || isBirthday && (speed >= 86)){
+	    return 2;
+	  }
+	}
+	public String alarmClock(int day, boolean vacation) {
+	  if(!vacation && (day >= 1 && day <= 6)){
+		    return "7:00";
+		  }
+		  if else (vacation && (day >= 1 && day <= 6)){
+		    return "10:00";
+		  }
+		  if else (!vacation && (day == 0 || day == 7)){
+		    return "10:00";
+		  }
+		  if else (vacation && (day == 0 || day == 7)) {
+		    return "off";
+		  }
+		  return -1;
 
 }
