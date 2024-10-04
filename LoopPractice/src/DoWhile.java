bfa504b401b1058c4dee4efe9fb141cc42c21adb
Courtney LaCroix <courtney.lacroix@outlook.com>
2024-10-04 13:23:30 -0600

    Do-While practice

---
+++ LoopPractice/src/DoWhile.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Please enter a number:");
+		int userNumber = scn.nextInt();
+		int totalSum = 0;
+		
+		do{
+			totalSum = totalSum + userNumber;
+			System.out.println("Please enter a another number\n"
+					+ "Enter 0 to exit");
+			
+			System.out.printf("current total: %d\n", totalSum);
+			
+			userNumber = scn.nextInt();
+		}
+		// Condition check
+		while (userNumber > 0);
+	}
+
+}
